AospExtended/platform_vendor_aosp

vendor: Add and ship pixel_2016 sysconfig
Signed-off-by: sb6596 <shubhamprince111@gmail.com>
Change-Id: I5c31aac01f5669e71d2b407945bbcd402c68ff0c

config/common.mk
@@ -123,6 +123,10 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/permissions/privapp-permissions-google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google.xml
 endif
 
+# Pixel sysconfig from Pixel XL (Photos)
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
+
 # Extra tools
 PRODUCT_PACKAGES += \
     e2fsck \

tmpaos/android_vendor_lineage

config/common.mk
@@ -164,6 +164,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
 
+# Pixel sysconfig from Pixel XL (Photos)
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
+
 # Extra tools in Lineage
 PRODUCT_PACKAGES += \
     7z \